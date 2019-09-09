192b8ec20cc933c69b5245c4f4c18380c05baf45
Neha Patel <neha.r.p@gmail.com>
2019-09-09 15:49:44 -0700

    POST account working

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -11,25 +11,44 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ error: 'Error retrieving accounts.' });
+      res.status(500).json({ error: "Error retrieving accounts." });
     });
 });
 
-router.post("/", (req, res) => {
-    const accountData = req.body;
+// router.get('/:id', (req, res) => {
+//     // select * from posts where id = 2
+//     const { id } = req.params;
 
-    db('accounts')
-        .insert(accountData, 'id')
-        .then(({ id }) => {
-            db('accounts')
-            .where({ id })
-            .then(account => {
-                res.status(200).json(account)
-            })
-        })
-        .catch(err => {
-            res.json(err)
-        })
-})
+//     db('posts')
+//         .where({ id }) // always returns an array
+//         .first() // picks the first element of the resulting array
+//         .then(posts => {
+//             res.status(200).json(posts);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
+
+router.get('/:id', ())
+
+router.post("/", (req, res) => {
+  const accountData = req.body;
+
+  db("accounts")
+    .insert(accountData, "id")
+    .then(([id]) => {
+      db("accounts")
+        .where({ id })
+        .first()
+        .then(account => {
+          res.status(200).json(account);
+        });
+    })
+    .catch(err => {
+      console.log("error: ", err);
+      res.status(500).json({ error: "Error creating account" });
+    });
+});
 
 module.exports = router;
